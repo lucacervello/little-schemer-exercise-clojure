f7e4565ae94e6c3d790a0ecb431bc53f7bbd05b3
lucacervello <luca.cervello@gmail.com>
2015-10-20 15:28:48 +0200

    Added lat? member? with test

--- src/little_schemer_exercise_clojure/core.clj
+++ src/little_schemer_exercise_clojure/core.clj
@@ -4,3 +4,26 @@
   "Atom in the meaning of Scheme e CommonLisp"
   [x]
   (not (coll? x)))
+
+(defn lat?
+  "lat? return true if the sequence is composed only by atoms"
+  [lst]
+  (cond
+    (nil? (seq lst)) true
+    (atom? (first lst)) (lat? (rest lst))
+    :else false))
+
+(defn my-member?
+  "return true if in the seq there is a"
+  [a lst]
+  (if (seq lst)
+    (if (= a (first lst))
+      true
+      (my-member? a (rest lst)))
+    false))
+
+(defn member? [a lst]
+  (if (seq lst)
+    (or (= a (first lst))
+        (member? a (rest lst)))
+    false))
--- test/little_schemer_exercise_clojure/core_test.clj
+++ test/little_schemer_exercise_clojure/core_test.clj
@@ -10,3 +10,17 @@
       (is (= true (atom? "a")))
       (is (= false (atom? [1 2])))
       (is (= false (atom? '(1 2 3)))))))
+
+(deftest lat?-test
+  (testing "lat? return true if the list is composed only by atom"
+    (do
+      (is (= true (lat? ["Jack" "Sprat" "could" "eat" "no" "chicken" "fat"])))
+      (is (= false (lat? [["Jack"] "Sprat" "could" "eat" "no" "chicken" "fat"])))
+      (is (= false (lat? ["Jack" ["Sprat" "could"] "eat" "no" "chicken" "fat"])))
+      (is (= true (lat? []))))))
+
+(deftest member?-test
+  (testing "member? return true if the argument is in the sequence"
+    (do
+      (is (= true (member? "tea" ["coffee" "tea" "or" "milk"])))
+      (is (= false (member? "poached" ["fried" "eggs" "and" "scrambled" "eggs"]))))))
